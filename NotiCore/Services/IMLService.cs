75a371b0bee399fa9412d2b60ca29d4ffe6c2d2f
Jorge Dominguez <jorge.dm97@gmail.com>
2021-02-01 22:37:19 -0700

    Wrapping model into service

---
+++ NotiCore/Services/IMLService.cs
@@ -0,0 +1,13 @@
+﻿using NotiCoreML.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotiCore.API.Services
+{
+    public interface IMLService
+    {
+        Task<ModelOutput> GetPredictionFromUrlAsync(string url);
+    }
+}
