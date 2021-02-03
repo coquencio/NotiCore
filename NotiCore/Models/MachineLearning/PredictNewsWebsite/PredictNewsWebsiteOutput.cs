60d7e1bd15e1cdddb8934820e32c4276edfa01ed
Jorge Dominguez <jorge.dm97@gmail.com>
2021-02-02 23:02:28 -0700

    Models reorganization

---
+++ NotiCore/Models/MachineLearning/PredictNewsWebsite/PredictNewsWebsiteOutput.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotiCore.API.Models.MachineLearning.PredictNewsWebsite
+{
+    public class PredictNewsWebsiteOutput
+    {
+        // ColumnName attribute is used to change the column name from
+        // its default value, which is the name of the field.
+        [ColumnName("PredictedLabel")]
+        public String Prediction { get; set; }
+        public float[] Score { get; set; }
+    }
+}
--- NotiCore/Models/Response/ResponseWrapper.cs
+++ NotiCore/Models/Response/ResponseWrapper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NotiCore.API.Infraestructure.Responses
+namespace NotiCore.API.Models.Responses
 {
     public class ResponseWrapper<T>
     {
