a3e93fff598e329d3ad348b6904e315244ee3a9b
Jorge Dominguez <jorge.dm97@gmail.com>
2021-02-06 23:16:39 -0700

    Handling validation errors and responses

---
+++ NotiCore/Helpers/ErrorsHelper.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotiCore.API.Helpers
+{
+    public static class ErrorsHelper
+    {
+        public static void ThrowValidationError(string message, string details = null, string field = null)
+        {
+            var failure = new ValidationFailure(field, details);
+            var errors = new List<ValidationFailure> { failure };
+            throw new ValidationException(message, errors);
+        }
+    }
+}
