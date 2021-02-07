b948d63c5bf2c03ca4e0beba6a91d83f59961829
Jorge Dominguez <jorge.dm97@gmail.com>
2021-02-06 23:14:38 -0700

    Automapper and fluent validation packages

---
+++ NotiCore/Infraestructure/Automapper/RequestsMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NotiCore.API.Models.DataContext;
+using NotiCore.API.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotiCore.API.Infraestructure.Automapper
+{
+    public class RequestsMappingProfile : Profile
+    {
+        public RequestsMappingProfile()
+        {
+            CreateMap<AddSourceRequest, Source> ();
+        }
+    }
+}
--- NotiCore/Startup.cs
+++ NotiCore/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NotiCore.API.Infraestructure.Automapper;
 using NotiCore.API.Models.DataContext;
 using NotiCore.API.Services;
 using NotiCore.API.Services.Implementation;
@@ -31,17 +33,13 @@ namespace NotiCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpClient<IScraperService, ScraperService>();
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new RequestsMappingProfile());
+            });
 
-            services.AddSingleton<IPredictNewsWebsiteService, PredictNewsWebsiteService>();
-            
-            services.AddTransient<IMLNewsWebsiteModel>(x=> new MLNewsWebsiteModel(@"../NotiCoreML.Model/MLModel.zip"));
-            File.Copy(@"Infraestructure/PythonLibs/newscatcher-0.2.0-py3-none-any.whl", @"Infraestructure/newscatcher-0.2.0-py3-none-any.whl", true);
-            PythonService.SetupModules(@"Infraestructure/PythonLibs/newscatcher-0.2.0-py3-none-any.whl");
-            File.Move(@"Infraestructure/newscatcher-0.2.0-py3-none-any.whl", @"Infraestructure/PythonLibs/newscatcher-0.2.0-py3-none-any.whl", true);
-
-            services.AddSingleton<IPythonService, PythonService>();
-            
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
             services.AddControllers()
                 .AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
@@ -49,6 +47,23 @@ namespace NotiCore
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NotiCore", Version = "v1" });
             });
             services.AddDbContext<DataContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DBContection")));
+
+            services.AddHttpClient<IScraperService, ScraperService>();
+
+            services.AddScoped<IPredictNewsWebsiteService, PredictNewsWebsiteService>();
+
+            services.AddScoped<ISourceService, SourceService>();
+
+            services.AddSingleton<IMLNewsWebsiteModel>(x => new MLNewsWebsiteModel(@"../NotiCoreML.Model/MLModel.zip"));
+            
+            // Python Setup
+            // To Do: Make file relocation dynamic
+            File.Copy(@"Infraestructure/PythonLibs/newscatcher-0.2.0-py3-none-any.whl", @"Infraestructure/newscatcher-0.2.0-py3-none-any.whl", true);
+            PythonService.SetupModules(@"Infraestructure/PythonLibs/newscatcher-0.2.0-py3-none-any.whl");
+            File.Move(@"Infraestructure/newscatcher-0.2.0-py3-none-any.whl", @"Infraestructure/PythonLibs/newscatcher-0.2.0-py3-none-any.whl", true);
+
+            services.AddSingleton<IPythonService, PythonService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
